267eb57031b17d6f4c5e1848e9508c0b3068ed96
Ryuichi Suzuki <untitled.tex@gmail.com>
2026-03-19 07:02:17 +0900

    Refactor Symbol class to remove context_uid and namespace attributes

--- lumie/symbol/symbol.py
+++ lumie/symbol/symbol.py
@@ -12,13 +12,9 @@ SYMBOL_NAMESPACE = uuid5(LUMIE_NAMESPACE, 'symbol')
 class Symbol:
     _NAMESPACE: ClassVar[UUID] = uuid5(SYMBOL_NAMESPACE, 'Symbol')
     _LOCK: ClassVar[RLock] = RLock()
-    _instances: ClassVar[WeakValueDictionary[tuple[str, UUID | None], Self]] = (
-        WeakValueDictionary()
-    )
+    _instances: ClassVar[WeakValueDictionary[str, Self]] = WeakValueDictionary()
 
-    __slots__ = ('context_uid', 'name', 'namespace', 'uid')
-    context_uid: UUID | None = None
-    namespace: UUID
+    __slots__ = ('name', 'uid')
     name: str
     uid: UUID
 
@@ -28,62 +24,38 @@ class Symbol:
         cls._instances = WeakValueDictionary()
 
     def validate_uid(self) -> None:
-        expected_namespace = (
-            uuid5(self._NAMESPACE, self.context_uid.hex)
-            if self.context_uid
-            else self._NAMESPACE
-        )
-        expected_uid = uuid5(expected_namespace, self.name)
-        if self.namespace != expected_namespace:
-            raise ValueError(
-                f"Namespace {self.namespace} does not match the expected namespace {expected_namespace} for name '{self.name}'"
-            )
+        expected_uid = uuid5(self._NAMESPACE, self.name)
         if self.uid != expected_uid:
             raise ValueError(
                 f"UID {self.uid} does not match the expected UID {expected_uid} for name '{self.name}'"
             )
 
     @classmethod
-    def _create_with_full_attrs(
-        cls, name: str, uid: UUID, namespace: UUID, context_uid: UUID | None = None
-    ) -> Self:
-        if (symbol := cls._instances.get((name, context_uid))) is not None:
+    def _create_with_full_attrs(cls, name: str, uid: UUID) -> Self:
+        if (symbol := cls._instances.get(name)) is not None:
             return symbol
 
         with cls._LOCK:
-            if (symbol := cls._instances.get((name, context_uid))) is not None:
+            if (symbol := cls._instances.get(name)) is not None:
                 return symbol
 
             symbol = super().__new__(cls)
             object.__setattr__(symbol, 'name', name)
             object.__setattr__(symbol, 'uid', uid)
-            object.__setattr__(symbol, 'namespace', namespace)
-            object.__setattr__(symbol, 'context_uid', context_uid)
-            cls._instances[(name, context_uid)] = symbol
+            cls._instances[name] = symbol
         symbol.validate_uid()
         return symbol
 
-    def __new__(cls, name: str, *, context_uid: UUID | None = None) -> Self:
-        if symbol := cls._instances.get((name, context_uid)):
-            return symbol
-
+    def __new__(cls, name: str) -> Self:
         with cls._LOCK:
-            if symbol := cls._instances.get((name, context_uid)):
+            if symbol := cls._instances.get(name):
                 return symbol
 
             symbol = super().__new__(cls)
-
-            namespace = (
-                uuid5(cls._NAMESPACE, context_uid.hex)
-                if context_uid
-                else cls._NAMESPACE
-            )
-            uid_ = uuid5(namespace, name)
+            uid_ = uuid5(cls._NAMESPACE, name)
             object.__setattr__(symbol, 'name', name)
             object.__setattr__(symbol, 'uid', uid_)
-            object.__setattr__(symbol, 'namespace', namespace)
-            object.__setattr__(symbol, 'context_uid', context_uid)
-            cls._instances[(name, context_uid)] = symbol
+            cls._instances[name] = symbol
 
         return symbol
 
@@ -102,12 +74,9 @@ class Symbol:
         return self.name
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.name!r}, context_uid={self.context_uid!r})'
+        return f'{self.__class__.__name__}({self.name!r})'
 
     def __reduce__(
         self,
     ) -> Any:
-        return (
-            self._create_with_full_attrs,
-            (self.name, self.uid, self.namespace, self.context_uid),
-        )
+        return (type(self)._create_with_full_attrs, (self.name, self.uid))
