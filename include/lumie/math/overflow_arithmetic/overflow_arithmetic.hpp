3d517dad2d83702c79f2856f998a30bcb313c2d0
Ryuichi Suzuki <untitled.tex@gmail.com>
2026-04-09 16:08:04 +0900

    Refactor overflow trait definitions for clarity and consistency across compilers

--- include/lumie/math/overflow_arithmetic/overflow_arithmetic.hpp
+++ include/lumie/math/overflow_arithmetic/overflow_arithmetic.hpp
@@ -2,14 +2,33 @@
 // for overflow traits
 #pragma once
 
+#include "fallback.hpp"
 #include "forward.hpp"
 
 #if defined(__GNUC__) || defined(__clang__)
 #include "gnu.hpp"
-#endif
 
-#if defined(_MSC_VER)
+namespace lumie::math::overflow_arithmetic {
+template <typename T>
+  requires std::is_integral_v<T> && std::is_signed_v<T>
+using default_overflow_trait = overflow_trait<T, gnu::tag>;
+} // namespace lumie::math::overflow_arithmetic
+
+#elif defined(_MSC_VER)
 #include "msvc.hpp"
-#endif
 
-#include "fallback.hpp"
+namespace lumie::math::overflow_arithmetic {
+template <typename T>
+  requires std::same_as<T, std::int16_t> || std::same_as<T, std::int32_t>
+using default_overflow_trait = overflow_trait<T, msvc::experimental_tag>;
+} // namespace lumie::math::overflow_arithmetic
+
+#else
+
+namespace lumie::math::overflow_arithmetic {
+template <typename T>
+  requires std::same_as<T, std::int16_t> || std::same_as<T, std::int32_t>
+using default_overflow_trait = overflow_trait<T, fallback::tag>;
+} // namespace lumie::math::overflow_arithmetic
+
+#endif
